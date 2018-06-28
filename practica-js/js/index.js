21ec622f32d9e2d705b09e9e833504b6d71e7504
DandyMateoHM <dmhachem87@gmail.com>
2018-06-28 16:38:47 -0500

    arreglo de js de practica-js

--- practica-js/js/index.js
+++ practica-js/js/index.js
@@ -1,34 +1,34 @@
-(function () {
+(function() {
 
-function cargado() {
-  var actual = 1
-var img = document.getElementById('imagen')
+    function cargado() {
+        var actual = 1
+        var img = document.getElementById('imagen')
 
-function cambiar() {
-  switch (actual) {
-    case 1:
-      actual = 2
-      img.src = 'img/002.png'
-      break
-    case 2:
-      actual = 3
-      img.src = 'img/003.png'
-      break
-    case 3:
-      actual = 4
-      img.src = 'img/004.png'
-      break
-    case 4:
-      actual = 1
-      img.src = 'img/001.png'
-      break
-  }
-}
+        function cambiar() {
+            switch (actual) {
+                case 1:
+                    actual = 2
+                    img.src = 'img/002.png'
+                    break
+                case 2:
+                    actual = 3
+                    img.src = 'img/003.png'
+                    break
+                case 3:
+                    actual = 4
+                    img.src = 'img/004.png'
+                    break
+                case 4:
+                    actual = 1
+                    img.src = 'img/001.png'
+                    break
+            }
+        }
 
-var btn = document.getElementById('boton')
-btn.onclick = cambiar
-}
+        var btn = document.getElementById('boton')
+        btn.onclick = cambiar
+    }
 
-window.onload = cargado
+    window.onload = cargado
 
 })()
